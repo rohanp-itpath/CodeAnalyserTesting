2e363fa09bd424be79ce2a25931459f77ac1d177
rohanp-itpath <rohanp.itpath@gmail.com>
2026-05-26 13:57:54 +0530

    Add the duplicate code logic

--- Condition.cs
+++ Condition.cs
@@ -4,15 +4,46 @@ namespace Testing;
 
 public class Condition
 {
-	private readonly bool _isFlag;
+    private readonly bool _isFlag;
 
-	public Condition(bool isFlag = true)
-	{
-		_isFlag = isFlag;
-	}
+    public Condition(bool isFlag = true)
+    {
+        _isFlag = isFlag;
+    }
 
-	public void CheckCondition()
-	{
-		Console.WriteLine(_isFlag ? "Condition is true." : "Condition is false.");
-	}
+    public void CheckCondition()
+    {
+        // Original logic
+        if (_isFlag)
+        {
+            Console.WriteLine("Condition is true.");
+        }
+        else
+        {
+            Console.WriteLine("Condition is false.");
+        }
+
+        // Duplicate logic block
+        if (_isFlag)
+        {
+            Console.WriteLine("Condition is true.");
+        }
+        else
+        {
+            Console.WriteLine("Condition is false.");
+        }
+    }
+
+    // Another duplicated method
+    public void ValidateCondition()
+    {
+        if (_isFlag)
+        {
+            Console.WriteLine("Condition is true.");
+        }
+        else
+        {
+            Console.WriteLine("Condition is false.");
+        }
+    }
 }
